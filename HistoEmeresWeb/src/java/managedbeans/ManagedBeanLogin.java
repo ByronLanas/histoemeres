73adec920182915c826596527252eeb76e2c9ae7
Battousai3@gmail.com <Battousai3@gmail.com@c2a925e1-dcec-8a82-7a53-c663a1f3b201>
2013-08-06 21:35:02 +0000

    Index con auto rediract a login managedbean login cambio logo

---
+++ HistoEmeresWeb/src/java/managedbeans/ManagedBeanLogin.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbeans;
+
+import java.io.IOException;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Battousai
+ */
+@Named(value = "managedBeanLogin")
+@RequestScoped
+public class ManagedBeanLogin {
+
+    /**
+     * Creates a new instance of ManagedBeanLogin
+     */
+    public ManagedBeanLogin() {
+        
+    }
+    public void irLogin() throws IOException{
+        FacesContext.getCurrentInstance().getExternalContext().redirect("faces/login.xhtml");
+    }
+}
