8644460d67589c5790b1b7d33050d0a2ba16df7a
Battousai3@gmail.com <Battousai3@gmail.com@c2a925e1-dcec-8a82-7a53-c663a1f3b201>
2013-08-11 01:40:02 +0000

    objetos web y menu

---
+++ trunk/HistoEmeresWeb/src/java/Objetos/Egreso.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author Battousai
+ */
+public class Egreso implements Serializable{
+    int codigo;
+    Date fecha;
+    String tipo;
+    float valor;
+}
