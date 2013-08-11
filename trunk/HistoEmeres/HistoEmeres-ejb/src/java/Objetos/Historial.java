87a2fdec3bc62084e9287cfa9815c2fe6ed7d5f0
Battousai3@gmail.com <Battousai3@gmail.com@c2a925e1-dcec-8a82-7a53-c663a1f3b201>
2013-08-11 01:41:10 +0000

    objetos ejb

---
+++ trunk/HistoEmeres/HistoEmeres-ejb/src/java/Objetos/Historial.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Battousai
+ */
+public class Historial implements Serializable{
+    
+}
