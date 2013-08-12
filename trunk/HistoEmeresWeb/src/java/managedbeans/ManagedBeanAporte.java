c515d2d30169357df6d51b71518d6960388d2e76
diegog.asd@gmail.com <diegog.asd@gmail.com@c2a925e1-dcec-8a82-7a53-c663a1f3b201>
2013-08-12 19:20:13 +0000

    Aporte LISTO (casi)

--- trunk/HistoEmeresWeb/src/java/managedbeans/ManagedBeanAporte.java
+++ trunk/HistoEmeresWeb/src/java/managedbeans/ManagedBeanAporte.java
@@ -4,8 +4,12 @@
  */
 package managedbeans;
 
+import Objetos.Aporte;
+import java.util.Date;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import sessionbeans.SessionBeanFinanaciero;
+import sessionbeans.SessionBeanIngreso;
 
 /**
  *
@@ -15,9 +19,63 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped
 public class ManagedBeanAporte {
 
+    private Aporte aporte;
+    private String municipio;
+    private Date fecha;
+    private float valor;
+
+    public Aporte getAporte() {
+        return aporte;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public void setAporte(Aporte aporte) {
+        this.aporte = aporte;
+    }
+
     /**
      * Creates a new instance of ManagedBeanAporte
      */
     public ManagedBeanAporte() {
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public int insertarAporte() {
+        boolean flag = verificarValor(valor);
+        if(!flag) return 0;
+        Aporte nuevoAporte = new Aporte(municipio, valor, fecha);
+        SessionBeanIngreso ingreso = new SessionBeanIngreso();
+        
+        return 1;
+    }
+
+    public boolean verificarValor(float valor) {
+        if (valor >= 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
