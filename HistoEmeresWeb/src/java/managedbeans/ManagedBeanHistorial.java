071d6d7641a0f3c9f125ce5b43580caf22692c14
Battousai3@gmail.com <Battousai3@gmail.com@c2a925e1-dcec-8a82-7a53-c663a1f3b201>
2013-08-12 00:22:21 +0000

    menu carga aportes usuarios, historiales e ingresar ventas paginas para CRUD aportes, CRUD usuarios, historiales e ingresar ventas

--- HistoEmeresWeb/src/java/managedbeans/ManagedBeanHistorial.java
+++ HistoEmeresWeb/src/java/managedbeans/ManagedBeanHistorial.java
@@ -4,8 +4,10 @@
  */
 package managedbeans;
 
+import java.io.Serializable;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import org.primefaces.model.chart.PieChartModel;
 
 /**
  *
@@ -13,11 +15,22 @@ import javax.enterprise.context.RequestScoped;
  */
 @Named(value = "managedBeanHistorial")
 @RequestScoped
-public class ManagedBeanHistorial {
+public class ManagedBeanHistorial implements Serializable{
 
-    /**
-     * Creates a new instance of ManagedBeanHistorial
-     */
-    public ManagedBeanHistorial() {
+    private PieChartModel pieModel; 
+    
+    public ManagedBeanHistorial()  {
+        crearPie();
+    }
+    public PieChartModel getPieModel() {  
+        return pieModel;  
+    } 
+    public void crearPie(){
+        pieModel = new PieChartModel();  
+        pieModel.set("Brand 1", 540);  
+        pieModel.set("Brand 2", 325);  
+        pieModel.set("Brand 3", 702);  
+        pieModel.set("Brand 4", 421);  
+     
     }
 }
---
+++ HistoEmeresWeb/src/java/managedbeans/ManagedBeanMenu.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbeans;
+
+import java.io.IOException;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Battousai
+ */
+@Named(value = "managedBeanMenu")
+@RequestScoped
+public class ManagedBeanMenu {
+
+    private static String pagina;
+    
+    public ManagedBeanMenu() {
+    }
+        public void irMenu(String pagina) throws IOException{
+        FacesContext.getCurrentInstance().getExternalContext().redirect("histoemeres.xhtml");
+        setPagina(pagina);
+    }
+    public String getPagina() {
+        return pagina;
+    }
+
+    public void setPagina(String pagina) {
+        this.pagina = pagina;
+    }        
+}
