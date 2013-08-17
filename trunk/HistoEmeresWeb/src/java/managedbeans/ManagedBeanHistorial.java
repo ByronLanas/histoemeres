7b08c62e6bfe6fe2fe35edb5fd4f64b6655eb4ed
Battousai3@gmail.com <Battousai3@gmail.com@c2a925e1-dcec-8a82-7a53-c663a1f3b201>
2013-08-17 16:54:33 +0000

    modificacion mensajes ingreso aporte

--- trunk/HistoEmeresWeb/src/java/managedbeans/ManagedBeanHistorial.java
+++ trunk/HistoEmeresWeb/src/java/managedbeans/ManagedBeanHistorial.java
@@ -32,14 +32,14 @@ import sessionbeans.VentaFacadeLocal;
  */
 @Named(value = "managedBeanHistorial")
 @SessionScoped
-public class ManagedBeanHistorial implements Serializable{
+public class ManagedBeanHistorial implements Serializable {
+
     @EJB
     private ProductoFacadeLocal productoFacade;
     @EJB
     private AporteFacadeLocal aporteFacade;
     @EJB
     private VentaFacadeLocal ventaFacade;
-
     private CartesianChartModel categoryModel;
     private int tipoGrafico;
     private int tipoHistorial;
@@ -49,11 +49,9 @@ public class ManagedBeanHistorial implements Serializable{
     private List<Venta> ventas;
     private List<Aporte> aportes;
     private List<Producto> productos;
-    
-    public ManagedBeanHistorial()  {
-        
-    }
 
+    public ManagedBeanHistorial() {
+    }
 
     public int getTipoGrafico() {
         return tipoGrafico;
@@ -110,19 +108,20 @@ public class ManagedBeanHistorial implements Serializable{
     public void setFin(Date fin) {
         this.fin = fin;
     }
-    
-    public Date fechaActual(){
+
+    public Date fechaActual() {
         return new Date();
     }
     /*
-    @PostConstruct
-    public void init(){
-            ventas=ventaFacade.findAll();
-            aportes=aporteFacade.findAll();
-            productos=productoFacade.findAll();
+     @PostConstruct
+     public void init(){
+     ventas=ventaFacade.findAll();
+     aportes=aporteFacade.findAll();
+     productos=productoFacade.findAll();
             
-    }
-*/
+     }
+     */
+
     public List<Producto> getProductos() {
         return productos;
     }
@@ -130,66 +129,124 @@ public class ManagedBeanHistorial implements Serializable{
     public void setProductos(List<Producto> productos) {
         this.productos = productos;
     }
-    
-    public void generarGrafico(ActionEvent actionEvent){
+
+    public void generarGrafico(ActionEvent actionEvent) {
         FacesContext context = FacesContext.getCurrentInstance();
         //context.addMessage(null, new FacesMessage("Successful", "inicio: "+inicio));
         //context.addMessage(null, new FacesMessage("Successful", "fin: "+ fin));
-        
-        
-        if(tipoHistorial==1){
-            
-            ventas=ventaFacade.findAll();
-            aportes=aporteFacade.findAll();
-            productos=productoFacade.findAll();
-            createCategoryModelAV();
+
+
+        switch (tipoHistorial) {
+            case 1:
+                ventas = ventaFacade.findAll();
+                aportes = aporteFacade.findAll();
+                productos = productoFacade.findAll();
+                //obtenerAportes(inicio,fin);
+
+                createCategoryModelAV();
+                break;
+            case 2:
+                aportes = aporteFacade.findAll();
             //obtenerAportes(inicio,fin);
-            //createCategoryModelAV();
-            
+            //createCategoryModelAPM();
+                break;
         }
     }
-    public CartesianChartModel getCategoryModel() {  
-        return categoryModel;  
+
+    public CartesianChartModel getCategoryModel() {
+        return categoryModel;
     }
-    private void createCategoryModelAV() {  
-        categoryModel = new CartesianChartModel();  
+
+    private void createCategoryModelAV() {
+        categoryModel = new CartesianChartModel();
         Aporte aporte;
         Venta venta;
-        float valor=1;
-        ChartSeries contributions = new ChartSeries();  
+        float valor = 1;
+        ChartSeries contributions = new ChartSeries();
         contributions.setLabel("Aportes");
-        
-        Iterator<Aporte> it= aportes.iterator();
-        SimpleDateFormat format=new SimpleDateFormat("yyyy-MM") ;
-        
-        while(it.hasNext()){
-            aporte=it.next();
+
+        Iterator<Aporte> it = aportes.iterator();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM");
+
+        while (it.hasNext()) {
+            aporte = it.next();
             contributions.set(format.format(aporte.getAportePK().getFechaMunicipalidad()), aporte.getValorAporte());
         }
-                
-        ChartSeries sales = new ChartSeries();  
-        sales.setLabel("Ventas");  
-  
-        Iterator<Venta> it2= ventas.iterator();
-        while(it2.hasNext()){
-            
-            venta=it2.next();
+
+        ChartSeries sales = new ChartSeries();
+        sales.setLabel("Ventas");
+
+        Iterator<Venta> it2 = ventas.iterator();
+        while (it2.hasNext()) {
+
+            venta = it2.next();
             /*
-            Iterator<Producto> it3=productos.iterator();
-            while(it3.hasNext()){
-                if(venta.getCodigoProducto()==it2){
-                    valor=it3.next().getValorProducto();
-                }
-            }*/
+             Iterator<Producto> it3=productos.iterator();
+             while(it3.hasNext()){
+             if(venta.getCodigoProducto()==it2){
+             valor=it3.next().getValorProducto();
+             }
+             }*/
             contributions.set(format.format(venta.getFechaVenta()), venta.getCantidadVenta());
         }
-        categoryModel.addSeries(contributions);  
-        categoryModel.addSeries(sales);    
+        categoryModel.addSeries(contributions);
+        categoryModel.addSeries(sales);
     }
-    public void obtenerVentas(Date start,Date end){
-        ventas=ventaFacade.buscarPorPeriodo(inicio, fin);
+
+    private void createCategoryModelAPM() {
+
+        Aporte aporte;
+        Venta venta;
+        float valor = 1;
+        ChartSeries contributions = new ChartSeries();
+        contributions.setLabel("Aportes");
+
+        Iterator<Aporte> it = aportes.iterator();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM");
+
+        while (it.hasNext()) {
+            aporte = it.next();
+            contributions.set(format.format(aporte.getAportePK().getFechaMunicipalidad()), aporte.getValorAporte());
+        }
+
+        ChartSeries sales = new ChartSeries();
+        sales.setLabel("Ventas");
+
+        Iterator<Venta> it2 = ventas.iterator();
+        while (it2.hasNext()) {
+
+            venta = it2.next();
+            /*
+             Iterator<Producto> it3=productos.iterator();
+             while(it3.hasNext()){
+             if(venta.getCodigoProducto()==it2){
+             valor=it3.next().getValorProducto();
+             }
+             }*/
+            contributions.set(format.format(venta.getFechaVenta()), venta.getCantidadVenta());
+        }
+        categoryModel.addSeries(contributions);
+        categoryModel.addSeries(sales);
     }
-    public void obtenerAportes(Date start,Date end){
+
+    public void obtenerVentas(Date start, Date end) {
+        ventas = ventaFacade.buscarPorPeriodo(inicio, fin);
+    }
+
+    public void obtenerAportes(Date start, Date end) {
         //aporteFacade
+    }
+
+    public class SwitchController {
+
+        private String value;
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(final String value) {
+            this.value = value;
+        }
     }
 }
