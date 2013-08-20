638f21187887e927147b627de47063d1ee1fca34
Battousai3@gmail.com <Battousai3@gmail.com@c2a925e1-dcec-8a82-7a53-c663a1f3b201>
2013-08-20 17:13:48 +0000

    requestscope

--- trunk/HistoEmeresWeb/src/java/managedbeans/ManagedBeanMenu.java
+++ trunk/HistoEmeresWeb/src/java/managedbeans/ManagedBeanMenu.java
@@ -5,6 +5,8 @@
 package managedbeans;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
@@ -19,20 +21,38 @@ import javax.faces.context.FacesContext;
 public class ManagedBeanMenu {
 
     private static String pagina;
-    
+
     public ManagedBeanMenu() {
     }
-    public void irMenu(String pagina) throws IOException{
-        
-        FacesContext.getCurrentInstance().getExternalContext().redirect("histoemeres.xhtml");
-        
-        setPagina(pagina);
+
+    public void irMenu(String pagina) {
+        try {
+            setPagina(pagina);
+            FacesContext.getCurrentInstance().getExternalContext().redirect("histoemeres.xhtml");
+            if (pagina.compareTo("mostrarUsuario") == 0) {
+                ManagedBeanUsuario mngbn = new ManagedBeanUsuario();
+                mngbn.init();
+            } else if (pagina.compareTo("mostrarClientes") == 0) {
+                ManagedBeanCliente mngbn = new ManagedBeanCliente();
+                mngbn.init();
+            } else if (pagina.compareTo("mostrarProductos") == 0) {
+                ManagedBeanProducto mngbn = new ManagedBeanProducto();
+                mngbn.init();
+            } else if (pagina.compareTo("mostrarAportes") == 0) {
+                ManagedBeanAporte mngbn = new ManagedBeanAporte();
+                mngbn.init();
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ManagedBeanMenu.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+
     public String getPagina() {
         return pagina;
     }
 
     public void setPagina(String pagina) {
         this.pagina = pagina;
-    }        
+    }
 }
